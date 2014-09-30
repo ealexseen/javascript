92f23b2754499aa013fce1443d7217cbb4854c47
Eugene Alexeenko <yauheni.aliakseyenko@neklo.com>
2014-09-30 13:54:52 +0300

    edit radius

--- js/radius.js
+++ js/radius.js
@@ -2,12 +2,15 @@ window.onload = function(){
 
     var canvas = document.getElementById('radius');
     var context = canvas.getContext('2d');
-    var radius = 80;
+    var radius = 20;
+    var dragging = false;
+
+
 
     var WH = function(){
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-    }
+    };
 
     /* height width (onload, resize) */
     WH();
@@ -15,20 +18,37 @@ window.onload = function(){
     $(window).resize(function(){
         WH();
     });
-
     /* end height width (onload, resize) */
+    
+    context.lineWidth = radius*2;
 
     var putPoint = function(e){
-        context.beginPath();
+        if(dragging){
+            context.lineTo(e.offsetX, e.offsetY);
+            context.stroke();
+            context.beginPath();
+            context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
+            context.fill();
+            context.beginPath();
+            context.moveTo(e.offsetX, e.offsetY);
+        }
+    };
 
-        context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
-
-        context.fill();
+    var engage = function(e){
+        dragging = true;
+        putPoint(e);
     }
 
-    canvas.addEventListener('mousedown', putPoint, false);
+    var disengage = function(e){
+        dragging = false;
+        context.beginPath();
+    }
 
-}
+    canvas.addEventListener('mousedown', engage, false);
+    canvas.addEventListener('mousemove', putPoint, false);
+    canvas.addEventListener('mouseup', disengage, false);
+
+};
 
 
 
